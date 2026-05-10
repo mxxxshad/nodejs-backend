dfc077ace918dbbd7cb6d073183d2fa0dd36a6ee
Mohamed Arshad <mxshadx@gmail.com>
2026-05-10 08:55:26 +0530

    added the migrations on the prisma

--- src/routes/movieRoutes.js
+++ src/routes/movieRoutes.js
@@ -3,6 +3,7 @@ import express from 'express';
 const router = express.Router();
 
 router.get("/" ,(req,res) => {
+    users.create("")
     res.json({httpMethod:"get"});
 });
 router.post("/" ,(req,res) => {
